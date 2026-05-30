4dab1e46811b08fb01db08e9a713fcfa9fdb7a0e
NICCOLI Samuel (ginahe) <samuel.niccoli.pro@gmail.com>
2026-05-30 17:17:55 +0200

    feat: new base cell

---
+++ script/cell/baseCell.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var ground : MeshInstance3D = $offset/ground
+
+var groundMeshBase = preload("res://Assets/mesh/groundMeshBase.tres")
+var groundMeshRich = preload("res://Assets/mesh/groundMeshRich.tres")
+var groundMeshWet = preload("res://Assets/mesh/groundMeshWet.tres")
+
+func initCell():
+	updateGround()
+	
+func updateGround():
+	pass
+
+func updateContent():
+	pass
