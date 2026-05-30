c598774ae0675e350dbcf323fba932d32f6b197a
SquixTech <6475960+Squix@users.noreply.github.com>
2026-05-30 14:31:38 +0200

    feat: gen aléatoire du parasite à l'init du jeu

--- script/parasite.gd
+++ script/parasite.gd
@@ -3,14 +3,18 @@ extends Node
 
 @onready var field : Map = %Map
 
-func spawn_parasite():
+func _spawn_parasite():
 	#get field
 	#get a random cell
 	#change state of this random cell
 	var random_cell : Cell = field.getCellUnsafe(randi_range(0, field.width), randi_range(0, field.height))
-	
+	random_cell.set_state(Cell.CellState.containParasite)
 func start_turn():
 	pass
 
 func end_turn():
 	pass
+
+
+func _on_game_manager_init_game() -> void:
+	_spawn_parasite()
