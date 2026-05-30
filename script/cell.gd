c1a2c30f5a96feea16c362a34f5af9987ae44289
SquixTech <6475960+Squix@users.noreply.github.com>
2026-05-30 12:43:39 +0200

    refactor : use enum for cell state

--- script/cell.gd
+++ script/cell.gd
@@ -1,7 +1,9 @@
 class_name Cell
 extends Node3D
 
-var containColza: bool  = true
+enum CellState {containColza, containParasite, dead}
+
+
 
 func KillColza():
 	$CSGBox3D.material.albedo_color = Color.RED
