72fb73a190a78f564d00aa3c83055db7749d282f
NICCOLI Samuel (ginahe) <samuel.niccoli.pro@gmail.com>
2026-05-30 19:15:24 +0200

    Go

--- script/player.gd
+++ script/player.gd
@@ -13,38 +13,11 @@ var currentTool : Player.tool =  Player.tool.none
 
 func start_turn():
 	print_debug("Start Player Turn")
-	%PlayerInput.connect("map_clicked",onMapClicked)
 	remainingAction = actionPerPhase
 
 func end_turn():
-	%PlayerInput.disconnect("map_clicked",onMapClicked)
 	pass
 
-
-
-func onMapClicked(_worldPosition:Vector3):
-	if(remainingAction <= 0):
-		return
-	
-	var coord = %Map.worldPosToCellCoord(_worldPosition)
-	var cell : Cell = %Map.getCellSafe(coord.x,coord.y)
-	
-	if(!cell):
-		return
-	
-	match currentTool:
-		Player.tool.none:
-			pass
-		Player.tool.scythe:
-			useScytheAt(cell)
-		Player.tool.watteringCan:
-			useWatteringCanAt(cell)
-		Player.tool.magnifyingGlass:
-			useMagnifyingGlassAt(cell)
-		Player.tool.tree:
-			useTreeAt(cell)
-	
-	remainingAction -= 1
 
 func useWatteringCanAt(_cell:Cell):
 	_cell.updateWaterLevel(1)
@@ -60,9 +33,36 @@ func useScytheAt(_cell:Cell):
 
 func _on_ui_new_tool_selected(newTool: Player.tool) -> void:
 	currentTool = newTool
+	print(currentTool)
 	pass # Replace with function body.
 
 
 func _on_ui_next_turn_pressed() -> void:
 	end_turn()
 	pass # Replace with function body.
+
+
+func _on_player_input_map_clicked(_worldPosition: Vector3) -> void:
+	if(remainingAction <= 0):
+		return
+	
+	var coord = %Map.worldPosToCellCoord(_worldPosition)
+	var cell : Cell = %Map.getCellSafe(coord.x,coord.y)
+	
+	if(!cell):
+		return
+	
+	
+	match currentTool:
+		Player.tool.none:
+			return
+		Player.tool.scythe:
+			useScytheAt(cell)
+		Player.tool.watteringCan:
+			useWatteringCanAt(cell)
+		Player.tool.magnifyingGlass:
+			useMagnifyingGlassAt(cell)
+		Player.tool.tree:
+			useTreeAt(cell)
+	
+	remainingAction -= 1
