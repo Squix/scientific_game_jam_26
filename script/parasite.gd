9ca91edbadb7371ba2709d2ef4961c2d1c11888b
SquixTech <6475960+Squix@users.noreply.github.com>
2026-05-30 14:53:32 +0200

    feat: parasite kills its current scene

--- script/parasite.gd
+++ script/parasite.gd
@@ -3,13 +3,21 @@ extends Node
 
 @onready var field : Map = %Map
 
+var current_cell : Cell
+
 func _spawn_parasite():
 	#get field
 	#get a random cell
 	#change state of this random cell
 	var random_cell : Cell = field.getCellUnsafe(randi_range(0, field.width), randi_range(0, field.height))
 	random_cell.set_state(Cell.CellState.containParasite)
+	current_cell = random_cell
+	
 func start_turn():
+	print("Parasite turn")
+	#kill current_cell
+	print("current_cell", current_cell)
+	current_cell.KillColza()
 	pass
 
 func end_turn():
